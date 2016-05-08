0bb70d757d2b8793cab7168e86b4293342072e66
Georg Lutz <georg@georglutz.de>
2016-05-08 09:28:06 +0200

    Add hash

--- copy_duplicity_backups.rb
+++ copy_duplicity_backups.rb
@@ -5,7 +5,7 @@
 class CopyDuplicityBackups < Formula
   desc "Copies most recent duplicity backup files"
   homepage "https://github.com/sercxanto/copy_duplicity_backups"
-  head "https://github.com/sercxanto/copy_duplicity_backups.git"
+  head "https://github.com/sercxanto/copy_duplicity_backups.git", :revision => "53dd674ccae765a509ebfd3af1c9be2a206bab0f"
   def install
     bin.install "copy_duplicity_backups.py"
   end
