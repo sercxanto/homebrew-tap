00f1962dbca2e2b180853c3c256f8065c3233f06
Georg Lutz <georg@georglutz.de>
2016-05-08 09:14:07 +0200

    Add msmtpq_notify

---
+++ msmtpq_notify.rb
@@ -0,0 +1,12 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class MsmtpqNotify < Formula
+  desc "Notifies desktop user if msmtpq has actually sent or enqueued mail"
+  homepage "https://github.com/sercxanto/msmtpq_notify"
+  head "https://github.com/sercxanto/msmtpq_notify"
+  def install
+    bin.install "msmtpq_notify.py"
+  end
+end
