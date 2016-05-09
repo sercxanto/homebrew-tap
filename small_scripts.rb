b22c25471077422b23d96e6ae74c8432d6746c7e
Georg Lutz <georg@georglutz.de>
2016-05-09 17:29:11 +0200

    URL also supports git checkouts

--- small_scripts.rb
+++ small_scripts.rb
@@ -5,7 +5,7 @@
 class SmallScripts < Formula
   desc "small scripts sercxanto"
   homepage "https://github.com/sercxanto/small_scripts"
-  head "https://github.com/sercxanto/small_scripts.git"
+  url "https://github.com/sercxanto/small_scripts.git", :using => :git, :revision => "19ef824b9f150a7d1dcc4b2633918288adff36d0"
   def install
     bin.install "check_video_length.sh"
   end
