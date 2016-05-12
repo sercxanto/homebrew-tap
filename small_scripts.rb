6a9909ccff4f930213a11584788d068622001e3d
Georg Lutz <georg@georglutz.de>
2016-05-12 17:17:41 +0200

    Add head version and disable bottle

--- small_scripts.rb
+++ small_scripts.rb
@@ -2,6 +2,10 @@ class SmallScripts < Formula
   desc "small scripts sercxanto"
   homepage "https://github.com/sercxanto/small_scripts"
   url "https://github.com/sercxanto/small_scripts.git", :tag => "v0.1.0", :revision => "19ef824b9f150a7d1dcc4b2633918288adff36d0"
+  head "https://github.com/sercxanto/small_scripts.git"
+
+  bottle :unneeded
+
   def install
     bin.install "check_video_length.sh"
   end
