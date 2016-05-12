450c6e99ab7512c88fb2ff6840f7a9371c213105
Georg Lutz <georg@georglutz.de>
2016-05-12 11:50:41 +0200

    Fix last audit hint

--- copy_duplicity_backups.rb
+++ copy_duplicity_backups.rb
@@ -1,7 +1,7 @@
 class CopyDuplicityBackups < Formula
   desc "Copies most recent duplicity backup files"
   homepage "https://github.com/sercxanto/copy_duplicity_backups"
-  url "https://github.com/sercxanto/copy_duplicity_backups.git", :revision => "53dd674ccae765a509ebfd3af1c9be2a206bab0f"
+  url "https://github.com/sercxanto/copy_duplicity_backups.git", :tag => "v0.1.0", :revision => "53dd674ccae765a509ebfd3af1c9be2a206bab0f"
   def install
     bin.install "copy_duplicity_backups.py"
   end
--- msmtpq_notify.rb
+++ msmtpq_notify.rb
@@ -1,7 +1,7 @@
 class MsmtpqNotify < Formula
   desc "Notifies desktop user if msmtpq has actually sent or enqueued mail"
   homepage "https://github.com/sercxanto/msmtpq_notify"
-  url "https://github.com/sercxanto/msmtpq_notify.git", :revision => "9d7676043eba96e07743ed565f0c6791b3e18a76"
+  url "https://github.com/sercxanto/msmtpq_notify.git", :tag => "v0.1.0", :revision => "9d7676043eba96e07743ed565f0c6791b3e18a76"
   def install
     bin.install "msmtpq_notify.py"
   end
