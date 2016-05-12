1eb9697842ab25ff0ca538e1f327cb7bb845865d
Georg Lutz <georg@georglutz.de>
2016-05-12 11:35:41 +0200

    Use tag and revision as advised by audit tool

--- small_scripts.rb
+++ small_scripts.rb
@@ -1,7 +1,7 @@
 class SmallScripts < Formula
   desc "small scripts sercxanto"
   homepage "https://github.com/sercxanto/small_scripts"
-  url "https://github.com/sercxanto/small_scripts.git", :revision => "19ef824b9f150a7d1dcc4b2633918288adff36d0"
+  url "https://github.com/sercxanto/small_scripts.git", :tag => "v0.1.0", :revision => "19ef824b9f150a7d1dcc4b2633918288adff36d0"
   def install
     bin.install "check_video_length.sh"
   end
