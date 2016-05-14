c9c81e8d2956aff955d5bbc5735b41d5f61976a8
Georg Lutz <georg@georglutz.de>
2016-05-14 19:59:48 +0200

    Switch to own fork with executable fixes

--- github-mirroring.rb
+++ github-mirroring.rb
@@ -1,7 +1,7 @@
 class GithubMirroring < Formula
   desc "Python script for mirroring all an organisation's GitHub repositories"
-  homepage "https://github.com/dragonfly-science/github-mirroring"
-  head "https://github.com/dragonfly-science/github-mirroring.git"
+  homepage "https://github.com/sercxanto/github-mirroring"
+  head "https://github.com/sercxanto/github-mirroring.git", :branch => "fixes"
   def install
     bin.install "github-mirror.py"
   end
