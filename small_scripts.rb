a93e827b9d930467207b12d635c3a63890a69dde
Georg Lutz <georg@georglutz.de>
2016-06-24 13:23:14 +0200

    Update small_scripts

--- small_scripts.rb
+++ small_scripts.rb
@@ -1,12 +1,12 @@
 class SmallScripts < Formula
   desc "small scripts sercxanto"
   homepage "https://github.com/sercxanto/small_scripts"
-  url "https://github.com/sercxanto/small_scripts.git", :tag => "v0.2.0", :revision => "8333ef04a7bda8462caacfbd039f92fd8e3e592a"
+  url "https://github.com/sercxanto/small_scripts.git", :tag => "v0.3.0", :revision => "efeb07b61f467e0ffbe56f9c5ea7ca30a969aa8a"
   head "https://github.com/sercxanto/small_scripts.git"
 
   bottle :unneeded
 
   def install
-    bin.install "check_video_length.sh", "maildir_trash.sh"
+    bin.install "check_video_length.sh", "maildir_trash.sh", "fix_fiducia_csv.py"
   end
 end
